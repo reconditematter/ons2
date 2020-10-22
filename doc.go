a7e074a37143b44d4f155237d4a93fbd1e483e4e
Leonid Kneller <recondite.matter@gmail.com>
2020-10-22 09:41:43 -0400

    Add copyright (c).

--- doc.go
+++ doc.go
@@ -1,2 +1,6 @@
+// Copyright (c) 2019-2020 Leonid Kneller. All rights reserved.
+// Licensed under the MIT license.
+// See the LICENSE file for full license information.
+
 // Package ons2 -- operations on points on the unit sphere S².
 package ons2
